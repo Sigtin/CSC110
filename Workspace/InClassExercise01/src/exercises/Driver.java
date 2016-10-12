835a7a800b9327af51e0f59af107a9932b764ec1
Austin Sigler <ASigler@student.neumont.edu>
2016-10-12 15:59:04 -0600

    Add new workspace files

---
+++ Workspace/InClassExercise01/src/exercises/Driver.java
@@ -0,0 +1,13 @@
+package exercises;
+
+import java.io.IOException;
+
+public class Driver {
+
+	//Manually declare the main method
+	//Pop quiz: What is the alternate term for main?
+		//ENTRY POINT
+	public static void main(String[] args) throws IOException{
+		DataTypeAnalyzer.run();
+	}
+}
