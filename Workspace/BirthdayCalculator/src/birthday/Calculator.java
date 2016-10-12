cedcbd025d9eb458d908d7c2261ddf42e9c630ad
Austin Sigler <ASigler@student.neumont.edu>
2016-10-12 13:58:11 -0600

    Add all workspace files

---
+++ Workspace/BirthdayCalculator/src/birthday/Calculator.java
@@ -0,0 +1,10 @@
+package birthday;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Workspace/SimpleMath/src/math/Simple.java
@@ -0,0 +1,20 @@
+package math;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		int x = 20;
+		int y = 35;
+		System.out.println(x + "+" + y + "=" + (x + y));
+		
+		System.out.println(x + "-" + y + "=" + (x - y));
+		
+		System.out.println(x + "*" + y + "=" + (x * y));
+		
+		System.out.println(x + "/" + y + "=" + (x / y));
+		
+		System.out.println(x + "%" + y + "=" + (x % y));
+
+	}
+
+}
